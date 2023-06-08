940777814259e043abcfebbddeb2eceb50357e19
Kyle Kroll <Kyle.Kroll@vu.com>
2023-06-08 18:04:09 -0500

    Completed assignment as described

--- classy_person.py
+++ classy_person.py
@@ -8,8 +8,24 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+    def increase_age(self):
+        self.age = self.age + 1
+    
+    def say_greeting(self):
+        print(f"Hello world! My name is {self.name}!")
+    
+    def count_to_age(self):
+        count = 0
+        while count < self.age:
+            count = count + 1
+            print(count)
 
 
 
 
-# You won't need to call anything here.
+
+# You won't need to call anything here
